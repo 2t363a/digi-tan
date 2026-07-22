8eaed70ae55b69c9bd9d3e2810e79278fbe678a6
2T363A <32044316+2t363a@users.noreply.github.com>
2026-07-22 07:39:56 -0700

    .

--- g-globe/globe.cpp
+++ g-globe/globe.cpp
@@ -12,10 +12,12 @@
 
 using Random = std::mt19937_64;
 
+static constexpr const double kAspect = 3840.0 / 2160.0;
+
 struct State {
     struct Point { double x; double y; };
 
-    struct Ball {
+    struct Snowball {
         double x;
         double y;
         double r;
@@ -26,30 +28,32 @@ struct State {
         bool exists = true;
     };
 
-    static constexpr const unsigned kM = 1'000;
-    static constexpr const unsigned kN = 96;
+    static constexpr const unsigned kM = 600;
+    static constexpr const unsigned kN = 192;
+
+    long n_mergers = 0;
 
     std::chrono::steady_clock::time_point t0;
     std::chrono::steady_clock::time_point t1;
 
     std::array<Point, kN> circle;
-    std::vector<Ball> balls1;
-    std::vector<Ball> balls2;
-    std::vector<Ball>* balls;
-    std::vector<Ball>* balls_next;
-
+    std::vector<Snowball> ball_storage[2];
+    int active = 0;
+    std::vector<Snowball>* snowballs[2];
 
     State() {
         t0 = std::chrono::steady_clock::now();
         t1 = std::chrono::steady_clock::now();
 
-        balls = &balls1;
-        balls_next = &balls2;
+        snowballs[0] = &ball_storage[0];
+        snowballs[1] = &ball_storage[1];
+        active = 0;
 
 #if 0
-        balls->push_back(Ball{.x = -0.5, .y = -0.5, .r = 0.02, .xdot = 0.067 * 0x1p-30 * 5, .ydot = 0.069 * 0x1p-30 * 5, .r3 = 0.02 * 0.02 * 0.02});
-        balls->push_back(Ball{.x = 0.5, .y = 0.5, .r = 0.02, .xdot = -0.067 * 0x1p-30 * 5, .ydot = -0.069 * 0x1p-30 * 5, .r3 = 0.02 * 0.02 * 0.02});
+    double r1 = 0.5;
+    snowballs[active]->push_back(Snowball{.x = 0.3, .y = 0, .r = r1, .xdot = 0.067 * 1, .ydot = 0, .r3 = r1 * r1 * r1});
 #endif
+
         double x = 1.0;
         double y = 0.0;
         double dc = cos(2 * M_PI / kN);
@@ -68,7 +72,10 @@ struct State {
     }
 
     void move(double dt) {
-        for (auto& ball: *balls) {
+        auto& balls = *snowballs[active];
+        /// wall collision
+        for (auto& ball: balls) {
+            if (not ball.exists) { continue; }
             double xdot = ball.xdot;
             double ydot = ball.ydot;
             double x = ball.x + dt * xdot;
@@ -78,19 +85,22 @@ struct State {
             do {
                 /// --- Правая сторона
                 if (x + r > 1.0) {
-                    x = 2 - (x + r) - r;
+                    double nx = 2.0 - x - r * 2;
+                    x = nx;
                     xdot = -xdot;
                 }
+                /// --- Левая
                 else if (x - r < -1.0) {
-                    x = -2.0 - (x - r) + r;
+                    double nx = -2.0 - x + r * 2;
+                    x = nx;
                     xdot = -xdot;
                 }
                 if (y + r > 1.0) {
-                    y = 2 - (y + r) - r;
+                    y = 2 - y - r * 2;
                     ydot = -ydot;
                 }
                 else if (y - r < -1.0) {
-                    y = -2.0 - (y - r) + r;
+                    y = -2.0 - y + r * 2;
                     ydot = -ydot;
                 }
                 ball.x = x;
@@ -99,59 +109,122 @@ struct State {
                 ball.ydot = ydot;
             } while (0);
         }
+
+        /// Плоское притяжение -- простой N^2
+        for (unsigned i = 0; i < balls.size(); ++i) {
+            double ex = 0.0;
+            double ey = 0.0;
+            if (not balls[i].exists) { continue; }
+            for (unsigned j = 0; j < balls.size(); ++j) {
+                if (not balls[j].exists) { continue; }
+                if (i == j) { continue; }
+                double dx = balls[i].x - balls[j].x;
+                double dy = balls[i].y - balls[j].y;
+                double r = dx * dx + dy * dy;
+                ex += dx * balls[j].r / r;
+                ey += dy * balls[j].r / r;
+            }
+        }
     }
 
     void physics() {
-        balls_next->clear();
-        auto &local_balls = *balls;
-        bool coalesce = true;
+        snowballs[!active]->clear();
+        auto &local_snowballs = *snowballs[active];
+        bool coalesce = false;
 
-        for (unsigned i = 0; i < local_balls.size(); ++i) {
-            coalesce = false;
-            auto ball = local_balls[i];
+        for (unsigned i = 0; i < local_snowballs.size(); ++i) {
+            auto ball = local_snowballs[i];
             if (not ball.exists) { continue; }
 
-            double x0 = ball.x;
-            double y0 = ball.y;
-            double r0 = ball.r;
-            double r30 = ball.r3;
+            double xi = ball.x;
+            double yi = ball.y;
+            double ri = ball.r;
 
-            for (unsigned j = 0; j < local_balls.size(); ++j) {
+            for (unsigned j = 0; j < local_snowballs.size(); ++j) {
                 if (j == i) { continue; }
-                auto ball2 = local_balls[j];
+                auto ball2 = local_snowballs[j];
                 if (not ball2.exists) { continue; }
 
-                double x1 = ball2.x;
-                double y1 = ball2.y;
-                double r1 = ball2.r;
-                double r31 = ball2.r3;
+                double xj = ball2.x;
+                double yj = ball2.y;
+                double rj = ball2.r;
 
-                double dx = x1 - x0;
-                double dy = y1 - y0;
-                if (fabs(dx) + fabs(dy) > r0 + r1) { continue; }
+                double rij = ri + rj;
+
+                double dx = xj - xi;
+                double dy = yj - yi;
+                if (fabs(dx) > rij) { continue; }
+                if (fabs(dy) > rij) { continue; }
 
                 double dx2 = dx * dx;
                 double dy2 = dy * dy;
-                if (sqrt(dx2 + dy2) < r0 + r1) {
+
+                if (sqrt(dx2 + dy2) < rij) {
                     coalesce = true;
-                    double xdot = (ball.xdot * r30 + ball2.xdot * r31) / (r30 + r31);
-                    double ydot = (ball.ydot * r30 + ball2.ydot * r31) / (r30 + r31);
+                    double x = (ball.x * ri + ball2.x * rj) / (rij);
+                    double y = (ball.y * ri + ball2.y * rj) / (rij);
+                    double xdot = (ball.xdot * ri + ball2.xdot * rj) / (rij);
+                    double ydot = (ball.ydot * ri + ball2.ydot * rj) / (rij);
 
-                    unsigned k = (r0 < r1) ? i : j;
-                    local_balls[k].exists = false;
+                    unsigned k = (ri >= rj) ? i : j;
+                    unsigned kk = (ri < rj) ? i : j;
+                    local_snowballs[k].exists = false;
+                    local_snowballs[kk].exists = false;
 
-                    ball.exists = true;
+                    n_mergers += 1;
+                    ball.x = x;
+                    ball.y = y;
                     ball.xdot = xdot;
                     ball.ydot = ydot;
-                    ball.r = cbrt(r30 + r31);
-                    ball.r3 = r30 + r31;
-                    if (ball.r > 0.5) { ball.r = 0.5; }
+                    double eff = 0.95;
+                    while (rij * eff < ri or rij * eff < rj) { eff = 1 - (1 - eff) / 2; }
+                    fprintf(stderr, "--- merger %ld\n", n_mergers);
+                    fprintf(stderr, "%lf and %lf merge with eff. %lf to form %lf\n", ri, rj, eff, eff * rij);
+                    fprintf(stderr, "--- merger\n");
+                    ball.r = eff * rij; /// energy loss
+                    ball.exists = true;
+                    if (ball.r > 1.0) { std::quick_exit(0); }
                     break;
                 }
             } // for (j = 0; ... )
-            balls_next->push_back(ball);
+            snowballs[!active]->push_back(ball);
         } // for (i = 0; ...)
-        std::swap(balls, balls_next);
+        active = !active;
+    }
+
+    void draw() {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE /* GL_FILL */);
+
+        for (auto const& ball : *snowballs[active]) {
+            if (not ball.exists) { continue; }
+
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+
+            glTranslatef(ball.x, ball.y, 0.0);
+            glScalef(ball.r, ball.r, 1.0);
+
+            glBegin(GL_POLYGON);
+            glColor3d(1.0, 1.0, 1.0);
+
+            for (int i = 0; i < circle.size(); ++i) {
+                glVertex2d(circle[i].x, circle[i].y);
+            }
+
+            glEnd();
+
+            glBegin(GL_POLYGON);
+            glColor3d(1.0, 1.0, 1.0);
+
+            for (int i = 0; i < circle.size(); ++i) {
+                glVertex2d(circle[i].x * 0.01, circle[i].y * 0.01);
+            }
+            glEnd();
+        }
+    }
+
+    void add_snowball(Snowball b) {
+        snowballs[active]->push_back(b);
     }
 }; /// struct State
 
@@ -161,19 +234,23 @@ static void
 init(Random& rng) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    double kWorld = 3;
-    glOrtho(-kWorld, kWorld, kWorld, -kWorld, -1.0, 1.0);
+    double kWorldX = 1.75;
+    double kWorldY = 1.5; 
 
+    glOrtho(-kAspect, kAspect, kWorldY, -kWorldY, -1.0, 1.0);
+
+    double sum_r3 = 0;
     for (int i = 0; i < State::kM; ++i) {
         double x = 1.0 - rng() * 0x1p-63;
         double y = 1.0 - rng() * 0x1p-63;
         double r = 0.01;
-        double xdot = (1.0 - rng() * 0x1p-64) * 0.25 * 0x1p-30;
-        double ydot = (1.0 - rng() * 0x1p-64) * 0.25 * 0x1p-30;
+        double xdot = (1.0 - rng() * 0x1p-64) * 0.25;
+        double ydot = (1.0 - rng() * 0x1p-64) * 0.25;
 
-        g_state.balls->push_back(State::Ball{.x = x, .y = y, .r = r, .xdot = xdot, .ydot = ydot, .r3 = r * r * r});
-    }
-}
+        sum_r3 += r * r * r;
+        g_state.add_snowball(State::Snowball{.x = x, .y = y, .r = r, .xdot = xdot, .ydot = ydot, .r3 = r * r * r});
+    } /// for (int i = 0; ...)
+} /// init()
 
 static void
 draw() {
@@ -205,26 +282,9 @@ draw() {
         glVertex2d(0, +1.0);
     glEnd();
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE /* GL_FILL */);
-
-    for (auto const& ball : *g_state.balls) {
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-
-        glTranslatef(ball.x, ball.y, 0.0);
-        glScalef(ball.r * 9.0 / 16.0, ball.r, 1.0);
-
-        glBegin(GL_POLYGON);
-        glColor3d(1.0, 1.0, 1.0);
-
-        for (int i = 0; i < g_state.circle.size(); ++i) {
-            glVertex2d(g_state.circle[i].x, g_state.circle[i].y);
-        }
-
-        glEnd();
-    }
-    glFlush();
-
+    /// draw state-dependent
+    g_state.draw();
+    // glFlush();
     glutSwapBuffers();
 }
 
@@ -245,7 +305,7 @@ idle() {
         std::this_thread::sleep_for(dt * 1ns);
     }
     g_state.t1 = t2;
-    g_state.move(dt);
+    g_state.move(dt * 0x1p-30);
     g_state.physics();
     glutPostRedisplay();
 }
